3ddb322f116bef61102e78f8fe20de2f9ab469c7
JoeyToh <joeytoh29@gmail.com>
2020-10-08 19:48:27 +0800

    Copy changes from Tag class over to a new Tag2 class Revert Tag class to original

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -1,13 +1,7 @@
 package seedu.address.model.tag;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
-import seedu.address.model.person.Name;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Tag in the address book.
@@ -15,83 +9,46 @@ import seedu.address.model.person.Name;
  */
 public class Tag {
 
-    // Identity fields
-    private final TagName name;
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
-    // Data fields
-    private final Set<Name> persons = new HashSet<>();
+    public final String tagName;
 
     /**
-     * Every field must be present and not null.
+     * Constructs a {@code Tag}.
+     *
+     * @param tagName A valid tag name.
      */
-    public Tag(TagName name, Set<Name> persons) {
-        requireAllNonNull(name, persons);
-        this.name = name;
-        this.persons.addAll(persons);
-    }
-
-    public TagName getTagName() {
-        return name;
+    public Tag(String tagName) {
+        requireNonNull(tagName);
+        checkArgument(isValidTagName(tagName), MESSAGE_CONSTRAINTS);
+        this.tagName = tagName;
     }
 
     /**
-     * Returns an immutable person set, which throws {@code UnsupportedOperationException}
-     * if modification is attempted.
+     * Returns true if a given string is a valid tag name.
      */
-    public Set<Name> getPersons() {
-        return Collections.unmodifiableSet(persons);
+    public static boolean isValidTagName(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
-    /**
-     * Returns true if both tags of the same name have at least one other identity field that is the same.
-     * This defines a weaker notion of equality between two tags.
-     */
-    public boolean isSameTag(Tag otherTag) {
-        if (otherTag == this) {
-            return true;
-        }
-
-        return otherTag != null && otherTag.getTagName().equals(getTagName());
-    }
-
-    /**
-     * Returns true if both tags have the same identity and data fields.
-     * This defines a stronger notion of equality between two tags.
-     */
     @Override
     public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (!(other instanceof Tag)) {
-            return false;
-        }
-
-        Tag otherTag = (Tag) other;
-        return otherTag.getTagName().equals(getTagName())
-                && otherTag.getPersons().equals(getPersons());
+        return other == this // short circuit if same object
+                || (other instanceof Tag // instanceof handles nulls
+                && tagName.equals(((Tag) other).tagName)); // state check
     }
 
     @Override
     public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, persons);
+        return tagName.hashCode();
     }
 
-    @Override
+    /**
+     * Format state as text for viewing.
+     */
     public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(getTagName())
-                .append(" Persons: ");
-        getPersons().forEach(builder::append);
-        return builder.toString();
+        return '[' + tagName + ']';
     }
-
-    // PLACEHOLDERS/QUICK FIX - DO NOT USE
-    public String tagName;
-    public static final String MESSAGE_CONSTRAINTS = "";
-    public Tag(String tagName) { this.name = new TagName(""); }
-    public static boolean isValidTagName(String tagName) { return true; }
 
 }
