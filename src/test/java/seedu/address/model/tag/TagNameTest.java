629b03ae20c855f62c52fa3bd2297f4ae9e6c61c
JoeyToh <joeytoh29@gmail.com>
2020-10-12 16:08:42 +0800

    Refactor TagTest to TagNameTest

--- src/test/java/seedu/address/model/tag/TagNameTest.java
+++ src/test/java/seedu/address/model/tag/TagNameTest.java
@@ -4,7 +4,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class TagTest {
+public class TagNameTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
