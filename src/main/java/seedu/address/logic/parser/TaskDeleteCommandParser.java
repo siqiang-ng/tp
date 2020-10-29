b17df4e55ba52faea20354dd9f4386ac6a1a0582
khinkhinn <2xkhin@gmail.com>
2020-10-30 01:26:40 +0800

    Follow checkstyle

--- src/main/java/seedu/address/logic/parser/TaskDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TaskDeleteCommandParser.java
@@ -5,8 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.tagcommands.TaskDeleteCommand;
-import seedu.address.logic.parser.Parser;
-import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class TaskDeleteCommandParser implements Parser<TaskDeleteCommand> {
