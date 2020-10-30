6b587f9776bf8c69f67eca34cf40eabc1423bc14
siqiang-ng <nsq.siqiang@gmail.com>
2020-10-30 23:30:33 +0800

    Add TaskDoneCommandTest

--- src/test/java/seedu/address/logic/commands/tagcommands/TaskDoneCommandTest.java
+++ src/test/java/seedu/address/logic/commands/tagcommands/TaskDoneCommandTest.java
@@ -1,7 +1,76 @@
-//package seedu.address.logic.commands.tagcommands;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class TaskDoneCommandTest {
-//
-//}
+package seedu.address.logic.commands.tagcommands;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_NAME_HANDBALL;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showTagAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TAG;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_TAG;
+import static seedu.address.testutil.TypicalProjact.getTypicalProjact;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.tag.Tag;
+
+class TaskDoneCommandTest {
+    private Model model = new ModelManager(getTypicalProjact(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalProjact(), new UserPrefs());
+
+    @Test
+    public void execute_invalidTagIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredTagList().size() + 1);
+        TaskDoneCommand taskDoneCommand = new TaskDoneCommand(outOfBoundIndex, INDEX_FIRST_TASK);
+
+        assertCommandFailure(taskDoneCommand, model, Messages.MESSAGE_INVALID_TAG_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidTaskIndexUnfilteredList_failure() {
+        Tag editedTag = model.getFilteredTagList().get(INDEX_FIRST_TAG.getZeroBased());
+        Index outOfBoundIndex = Index.fromOneBased(editedTag.getTagTasks().size() + 1);
+        TaskDoneCommand taskDoneCommand = new TaskDoneCommand(INDEX_FIRST_TAG, outOfBoundIndex);
+
+        assertCommandFailure(taskDoneCommand, model, Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidTagIndexFilteredList_failure() {
+        showTagAtIndex(model, INDEX_FIRST_TAG);
+        Index outOfBoundIndex = INDEX_SECOND_TAG;
+        // ensures that outOfBoundIndex is still in bounds of Projact list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getProjact().getTagList().size());
+
+        TaskDoneCommand TaskDoneCommand = new TaskDoneCommand(outOfBoundIndex, INDEX_FIRST_TASK);
+
+        assertCommandFailure(TaskDoneCommand, model, Messages.MESSAGE_INVALID_TAG_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        TaskDoneCommand commandOne = new TaskDoneCommand(Index.fromZeroBased(1), Index.fromZeroBased(1));
+        TaskDoneCommand commandTwo = new TaskDoneCommand(Index.fromZeroBased(0), Index.fromZeroBased(1));
+        TaskDoneCommand commandThree = new TaskDoneCommand(Index.fromZeroBased(1), Index.fromZeroBased(0));
+
+        // same object -> returns true
+        assertTrue(commandOne.equals(commandOne));
+
+        // different types -> returns false
+        assertFalse(commandOne.equals(1));
+
+        // null -> returns false
+        assertFalse(commandOne.equals(null));
+
+        // different index -> returns false
+        assertFalse(commandOne.equals(commandTwo));
+
+        // different task index -> return false
+        assertFalse(commandOne.equals(commandThree));
+    }
+}
