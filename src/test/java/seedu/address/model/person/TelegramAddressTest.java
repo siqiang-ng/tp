098a57eae3867e7ac3ba32c9e12b0181c085fd6a
pockii <yeokexin2@gmail.com>
2020-11-04 02:31:02 +0800

    Fix telegram address error message and add tests

--- src/test/java/seedu/address/model/person/TelegramAddressTest.java
+++ src/test/java/seedu/address/model/person/TelegramAddressTest.java
@@ -30,20 +30,25 @@ public class TelegramAddressTest {
 
         // invalid telegram address
         assertFalse(TelegramAddress.isValidTelegramAddress("jake")); // 4 characters
+        assertFalse(TelegramAddress.isValidTelegramAddress("j123456789012345678901234567890ee")); // 33 characters
         assertFalse(TelegramAddress.isValidTelegramAddress("j!kee")); // special characters
         assertFalse(TelegramAddress.isValidTelegramAddress("j@kee")); // special characters
         assertFalse(TelegramAddress.isValidTelegramAddress("j#kee")); // special characters
         assertFalse(TelegramAddress.isValidTelegramAddress("j.kee")); // special characters
         assertFalse(TelegramAddress.isValidTelegramAddress("peter jack")); // spaces in name
-        assertFalse(TelegramAddress.isValidTelegramAddress("_jake1")); // underscore at first letter
-        assertFalse(TelegramAddress.isValidTelegramAddress("jake1_")); // underscore at last letter
+        assertFalse(TelegramAddress.isValidTelegramAddress("1jake")); // number ae first letter
+        assertFalse(TelegramAddress.isValidTelegramAddress("_jake1")); // underscore as first character
+        assertFalse(TelegramAddress.isValidTelegramAddress("jake1_")); // underscore as last character
 
 
         // valid telegram address
         assertTrue(TelegramAddress.isValidTelegramAddress("jakee")); // alphabets only
-        assertTrue(TelegramAddress.isValidTelegramAddress("12345")); // numbers only
+        assertTrue(TelegramAddress.isValidTelegramAddress("a1234")); // numbers only
         assertTrue(TelegramAddress.isValidTelegramAddress("a___b")); //underscores only
         assertTrue(TelegramAddress.isValidTelegramAddress("jake123")); // alphanumeric
         assertTrue(TelegramAddress.isValidTelegramAddress("jak_123")); // mixture of alphanumeric and underscores
+        assertTrue(TelegramAddress.isValidTelegramAddress("j___e")); // alphabet as first and last character
+        assertTrue(TelegramAddress.isValidTelegramAddress("j___1")); // alphabet as first and number as last character
+        assertTrue(TelegramAddress.isValidTelegramAddress("j1234567890123456789012345678901")); // 32 characters
     }
 }
